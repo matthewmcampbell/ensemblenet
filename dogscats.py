794073b4e0b7e25690ef8382a55c5b7db371bf83
Kylexi <mmcampbell0@gmail.com>
2020-06-22 19:46:30 -0400

    Updated functionality for different conv channel out sizes

--- dogscats.py
+++ dogscats.py
@@ -1,21 +1,43 @@
-import tensorflow.keras as keras
+'''
+Acknowledging Kaggle contributor Uysim (http://wwww.kaggle.com/uysimty)
+Uysim's Kaggle kernel was utilized as reference for handling
+the dogs vs cats dataset and cleaning steps. Thanks Uysim.
+'''
+
+import os
 import numpy as np
 import pandas as pd
+import tensorflow as tf
+import tensorflow.keras as keras
 from tensorflow.keras.utils import to_categorical
 from tensorflow.keras.preprocessing.image import ImageDataGenerator
 from sklearn.model_selection import train_test_split
-import os
 
 from ensemblenet import EnsembleModel
+from tensorflow.compat.v1 import ConfigProto, Session, RunOptions
+from tensorflow.compat.v1.keras.backend import set_session
+import tensorflow as tf
+
+tf.keras.fit_verbose = 2
+
+RunOptions.report_tensor_allocations_upon_oom = True
+config = ConfigProto()
+config.gpu_options.allow_growth = True  # dynamically grow the memory used on the GPU
+config.log_device_placement = True  # to log device placement (on which device the operation ran)
+sess = Session(config=config)
+set_session(sess)  # set this TensorFlow session as the default session for Keras
+
+
 
 np.random.seed(0)
 IMAGE_WIDTH=128
 IMAGE_HEIGHT=128
 IMAGE_SIZE=(IMAGE_WIDTH, IMAGE_HEIGHT)
 IMAGE_CHANNELS=3
-path = 'c:/users/matthew/projects/data/dogs-vs-cats/train/train/'
-filenames = os.listdir(path)
+train_path = 'c:/users/matthew/projects/data/dogs-vs-cats/train/train/'
+test_path = 'c:/users/matthew/projects/data/dogs-vs-cats/test1/test1/'
 
+filenames = os.listdir(train_path)
 categories = []
 for filename in filenames:
     category = filename.split('.')[0]
@@ -25,14 +47,14 @@ for filename in filenames:
         categories.append(0)
 file_df = pd.DataFrame({'filename': filenames, 'category': categories})
 file_df['category'].replace({0: 'cat', 1: 'dog'}, inplace=True)
-file_df = file_df.sample(frac = 0.05)
+# file_df = file_df.sample(frac = 0.5)
 train_df, val_df = train_test_split(file_df, test_size=0.2, random_state=0)
 train_df.reset_index(drop=True, inplace=True)
 val_df.reset_index(drop=True, inplace=True)
 
 total_train = train_df.shape[0]
 total_validate = val_df.shape[0]
-batch_size=32
+batch_size=16
 
 train_datagen = ImageDataGenerator(
     rotation_range=15,
@@ -45,33 +67,37 @@ train_datagen = ImageDataGenerator(
 )
 train_generator = train_datagen.flow_from_dataframe(
     train_df, 
-    path, 
+    train_path, 
     x_col='filename',
     y_col='category',
     target_size=IMAGE_SIZE,
     class_mode='categorical',
-    batch_size=32
+    batch_size=batch_size
 )
 val_datagen = ImageDataGenerator(rescale=1./255)
 val_generator = val_datagen.flow_from_dataframe(
     val_df, 
-    path, 
+    train_path, 
     x_col='filename',
     y_col='category',
     target_size=IMAGE_SIZE,
     class_mode='categorical',
-    batch_size=32
+    batch_size=batch_size
 )
 
+
 num_classes = 2
-# input_shape = (28, 28, 1)
+num_sub_models = 10
+num_dense_layers = 3
 
 compile_params = {
     'loss': "categorical_crossentropy",
     'optimizer': 'adam',
     'metrics': ['accuracy']
 }
-shapes = list(map(tuple, np.random.randint(200, 2000, (3, 3))))
+
+dense_shapes = list(map(lambda x: sorted(tuple(x)), np.random.randint(200, 2000, (num_sub_models, num_dense_layers))))
+conv_shapes = list(map(lambda x: sorted(tuple(2**x)), np.random.randint(2, 6, (num_sub_models, 2))))
 compile_params = {
     'loss': "categorical_crossentropy",
     'optimizer': 'RMSprop',
@@ -79,10 +105,11 @@ compile_params = {
     }
 fit_params = (
     [train_generator], 
-    {'epochs': 10,
+    {'epochs': 5,
     'validation_data': val_generator,
     'validation_steps': total_validate//batch_size,
     'steps_per_epoch': total_train//batch_size,
+    'verbose': 2,
     # 'callbacks': callbacks,
     }
     )
@@ -92,17 +119,32 @@ fit_params2 = (
     'validation_data': val_generator,
     'validation_steps': total_validate//batch_size,
     'steps_per_epoch': total_train//batch_size,
+    'verbose': 2,
     # 'callbacks': callbacks,
     }
     )
 
-model = EnsembleModel(shapes, num_classes, compile_params, fit_params)
+model = EnsembleModel(dense_shapes, num_classes, compile_params, fit_params, conv_shapes)
 model.compile(**compile_params)
 model.fit(*fit_params2[0], **fit_params2[1])
 
+test_files = os.listdir(test_path)
+test_df = pd.DataFrame({'filename': test_files})
+test_datagen = ImageDataGenerator(rescale = 1./255)
+test_generator = test_datagen.flow_from_dataframe(
+    test_df,
+    test_path,
+    x_col='filename',
+    y_col=None,
+    class_mode=None,
+    target_size=IMAGE_SIZE,
+    batch_size=batch_size,
+    shuffle=False
+    )
+
 # Model Assessment
-model.get_model_accuracies(x_test, y_test)
-m = keras.metrics.CategoricalAccuracy()
-m.update_state(model(x_test), y_test)
-print("Full model.")
-print(m.result().numpy())
+# model.get_model_accuracies(x_test, y_test)
+# m = keras.metrics.CategoricalAccuracy()
+# m.update_state(model(x_test), y_test)
+# print("Full model.")
+# print(m.result().numpy())
--- ensemblenet.py
+++ ensemblenet.py
@@ -2,14 +2,21 @@ import tensorflow as tf
 import tensorflow.keras as keras
 from tensorflow.keras import layers
 
+default_conv_features = [16, 32]
+
 class EnsembleLayer(layers.Layer):
 
-	def __init__(self, dense_units, num_classes):
+	def __init__(self, dense_units, num_classes, conv_features = None):
 		super(EnsembleLayer, self).__init__()
 		# self.inp = keras.Input()
-		self.conv1 = layers.Conv2D(32, kernel_size=(3, 3), activation="relu")
+		if conv_features is None:
+			conv_features = default_conv_features
+		print(conv_features)
+		assert len(conv_features) == 2
+		c1, c2 = conv_features
+		self.conv1 = layers.Conv2D(c1, kernel_size=(3, 3), activation="relu")
 		self.pool1 = layers.MaxPooling2D(pool_size=(2, 2))
-		self.conv2 = layers.Conv2D(64, kernel_size=(3, 3), activation="relu")
+		self.conv2 = layers.Conv2D(c2, kernel_size=(3, 3), activation="relu")
 		self.pool2 = layers.MaxPooling2D(pool_size=(2, 2))
 		self.flat = layers.Flatten()
 		self.denses = [layers.Dense(d, activation="relu") for d in dense_units]
@@ -32,22 +39,29 @@ class EnsembleLayer(layers.Layer):
 
 class EnsembleUnit(keras.Model):
 
-	def __init__(self, dense_units, num_classes):
+	def __init__(self, dense_units, num_classes, conv_features = None):
 		super(EnsembleUnit, self).__init__()
-		self.block = EnsembleLayer(dense_units, num_classes)
+		self.block = EnsembleLayer(dense_units, num_classes, conv_features)
 
 	def call(self, inputs):
 		return self.block(inputs)
 
 class EnsembleModel(keras.Model):
 
-	def __init__(self, many_dense_units, num_classes, compile_params, fit_params):
+	def __init__(self, many_dense_units, num_classes, compile_params, fit_params, many_conv_features = None):
 		super(EnsembleModel, self).__init__()
+		if many_conv_features is None:
+			many_conv_features = [None]*len(many_dense_units)
+		assert len(many_dense_units) == len(many_conv_features)
+
 		self.many_dense_units = many_dense_units
-		self.submodels = [EnsembleUnit(dense_units, num_classes) for dense_units in many_dense_units]
+		self.submodels = [EnsembleUnit(dense_units, num_classes, conv_features) for dense_units, conv_features in zip(many_dense_units, many_conv_features)]
 		[model.compile(**compile_params) for model in self.submodels]
 		# [model.fit(*fit_params[0], **fit_params[1]) for model in self.submodels]
-		for model in self.submodels:
+		for i, model in enumerate(self.submodels):
+			print('Structure of submodel {}:'.format(i+1))
+			print('\tConv shapes: {}'.format(many_conv_features[i])) if not (many_conv_features[i] is None) else print('\tConv shapes: {}'.format(default_conv_features)) 
+			print('\tDense shapes: {}'.format(many_dense_units[i]))
 			model.fit(*fit_params[0], **fit_params[1])
 		for model in self.submodels:
 			model.trainable = False
