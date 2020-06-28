4aba2a9ca798295dc71acd1ba05c80c2460e4ed9
Kylexi <mmcampbell0@gmail.com>
2020-06-28 00:55:55 -0400

    Reformatted files to PEP8. Altered width/height shift from 0.1 to 0.2

--- dogscats.py
+++ dogscats.py
@@ -22,18 +22,19 @@ tf.keras.fit_verbose = 2
 
 RunOptions.report_tensor_allocations_upon_oom = True
 config = ConfigProto()
-config.gpu_options.allow_growth = True  # dynamically grow the memory used on the GPU
-config.log_device_placement = True  # to log device placement (on which device the operation ran)
+# dynamically grow the memory used on the GPU
+config.gpu_options.allow_growth = True
+# to log device placement (on which device the operation ran)
+config.log_device_placement = True
+# set this TensorFlow session as the default session for Keras
 sess = Session(config=config)
-set_session(sess)  # set this TensorFlow session as the default session for Keras
-
-
+set_session(sess)
 
 np.random.seed(0)
-IMAGE_WIDTH=128
-IMAGE_HEIGHT=128
-IMAGE_SIZE=(IMAGE_WIDTH, IMAGE_HEIGHT)
-IMAGE_CHANNELS=3
+IMAGE_WIDTH = 128
+IMAGE_HEIGHT = 128
+IMAGE_SIZE = (IMAGE_WIDTH, IMAGE_HEIGHT)
+IMAGE_CHANNELS = 3
 train_path = 'c:/users/matthew/projects/data/dogs-vs-cats/train/train/'
 test_path = 'c:/users/matthew/projects/data/dogs-vs-cats/test1/test1/'
 
@@ -45,92 +46,104 @@ for filename in filenames:
         categories.append(1)
     else:
         categories.append(0)
-        
+
 file_df = pd.DataFrame({'filename': filenames, 'category': categories})
 file_df['category'].replace({0: 'cat', 1: 'dog'}, inplace=True)
-file_df = file_df.sample(frac = 0.05)
+file_df = file_df.sample(frac=0.05)
 train_df, val_df = train_test_split(file_df, test_size=0.2, random_state=0)
 train_df.reset_index(drop=True, inplace=True)
 val_df.reset_index(drop=True, inplace=True)
 
 total_train = train_df.shape[0]
 total_validate = val_df.shape[0]
-batch_size=16
+batch_size = 16
 
 train_datagen = ImageDataGenerator(
     rotation_range=15,
-    rescale=1./255,
+    rescale=1. / 255,
     shear_range=0.1,
     zoom_range=0.2,
     horizontal_flip=True,
-    width_shift_range=0.1,
-    height_shift_range=0.1
+    width_shift_range=0.2,  # 0.1
+    height_shift_range=0.2  # 0.1
 )
 train_generator = train_datagen.flow_from_dataframe(
-    train_df, 
-    train_path, 
+    train_df,
+    train_path,
     x_col='filename',
     y_col='category',
     target_size=IMAGE_SIZE,
     class_mode='categorical',
     batch_size=batch_size
 )
-val_datagen = ImageDataGenerator(rescale=1./255)
+val_datagen = ImageDataGenerator(rescale=1. / 255)
 val_generator = val_datagen.flow_from_dataframe(
-    val_df, 
-    train_path, 
+    val_df,
+    train_path,
     x_col='filename',
     y_col='category',
     target_size=IMAGE_SIZE,
     class_mode='categorical',
     batch_size=batch_size
 )
-
 
 num_classes = 2
 num_sub_models = 3
 num_dense_layers = 2
-dense_shapes = list(map(lambda x: sorted(tuple(x)), np.random.randint(200, 2000, (num_sub_models, num_dense_layers))))
-conv_shapes = list(map(lambda x: sorted(tuple(2**x)), np.random.randint(2, 6, (num_sub_models, 2))))
+dense_shapes = list(map(
+    lambda x: sorted(tuple(x)),
+    np.random.randint(200, 2000, (num_sub_models, num_dense_layers))
+))
+conv_shapes = list(map(
+    lambda x: sorted(tuple(2 ** x)),
+    np.random.randint(2, 6, (num_sub_models, 2))
+))
 
 log_dir_sub = "./logs/fit/submodels/"
 log_dir_main = "./logs/fit/main/" + dt.datetime.now().strftime("%Y%m%d-%H%M%S")
-tensorboard_callback_sub = tf.keras.callbacks.TensorBoard(log_dir=log_dir_sub, histogram_freq=1, profile_batch=0)
-tensorboard_callback_main = tf.keras.callbacks.TensorBoard(log_dir=log_dir_main, histogram_freq=1)
+tensorboard_callback_sub = tf.keras.callbacks.TensorBoard(
+    log_dir=log_dir_sub, histogram_freq=1, profile_batch=0
+)
+tensorboard_callback_main = tf.keras.callbacks.TensorBoard(
+    log_dir=log_dir_main, histogram_freq=1
+)
 
 compile_params = {
     'loss': "categorical_crossentropy",
     'optimizer': 'RMSprop',
     'metrics': ['accuracy']
-    }
+}
 fit_params = (
-    [train_generator], 
+    [train_generator],
     {'epochs': 3,
-    'validation_data': val_generator,
-    'validation_steps': total_validate//batch_size,
-    'steps_per_epoch': total_train//batch_size,
-    'verbose': 2,
-    'callbacks': [tensorboard_callback_sub],
-    }
+     'validation_data': val_generator,
+     'validation_steps': total_validate // batch_size,
+     'steps_per_epoch': total_train // batch_size,
+     'verbose': 2,
+     'callbacks': [tensorboard_callback_sub],
+     }
 )
 fit_params2 = (
     [train_generator],
     {'epochs': 100,
-    'validation_data': val_generator,
-    'validation_steps': total_validate//batch_size,
-    'steps_per_epoch': total_train//batch_size,
-    'verbose': 2,
-    'callbacks': [tensorboard_callback_main],
-    }
+     'validation_data': val_generator,
+     'validation_steps': total_validate // batch_size,
+     'steps_per_epoch': total_train // batch_size,
+     'verbose': 2,
+     'callbacks': [tensorboard_callback_main],
+     }
 )
 
-model = EnsembleModel(dense_shapes, num_classes, compile_params, fit_params, conv_shapes, trainable=False)
+model = EnsembleModel(
+    dense_shapes, num_classes, compile_params,
+    fit_params, conv_shapes, trainable=False
+)
 model.compile(**compile_params)
 model.fit(*fit_params2[0], **fit_params2[1])
 
 test_files = os.listdir(test_path)
 test_df = pd.DataFrame({'filename': test_files})
-test_datagen = ImageDataGenerator(rescale = 1./255)
+test_datagen = ImageDataGenerator(rescale=1. / 255)
 test_generator = test_datagen.flow_from_dataframe(
     test_df,
     test_path,
@@ -140,7 +153,7 @@ test_generator = test_datagen.flow_from_dataframe(
     target_size=IMAGE_SIZE,
     batch_size=batch_size,
     shuffle=False
-    )
+)
 
 # Model Assessment
 # model.get_model_accuracies(x_test, y_test)
--- ensemblenet.py
+++ ensemblenet.py
@@ -4,78 +4,87 @@ from tensorflow.keras import layers
 
 default_conv_features = [16, 32]
 
+
 class EnsembleLayer(layers.Layer):
 
-	def __init__(self, dense_units, num_classes, conv_features = None):
-		super(EnsembleLayer, self).__init__()
-		if conv_features is None:
-			conv_features = default_conv_features
-		print(conv_features)
-		assert len(conv_features) == 2
-		c1, c2 = conv_features
-		self.conv1 = layers.Conv2D(c1, kernel_size=(3, 3), activation="relu")
-		self.pool1 = layers.MaxPooling2D(pool_size=(2, 2))
-		self.conv2 = layers.Conv2D(c2, kernel_size=(3, 3), activation="relu")
-		self.pool2 = layers.MaxPooling2D(pool_size=(2, 2))
-		self.flat = layers.Flatten()
-		self.denses = [layers.Dense(d, activation="relu") for d in dense_units]
-		self.dropout = layers.Dropout(0.5)
-		self.probs = layers.Dense(num_classes, activation="softmax")
+    def __init__(self, dense_units, num_classes, conv_features=None):
+        super(EnsembleLayer, self).__init__()
+        if conv_features is None:
+            conv_features = default_conv_features
+        print(conv_features)
+        assert len(conv_features) == 2
+        c1, c2 = conv_features
+        self.conv1 = layers.Conv2D(c1, kernel_size=(3, 3), activation="relu")
+        self.pool1 = layers.MaxPooling2D(pool_size=(2, 2))
+        self.conv2 = layers.Conv2D(c2, kernel_size=(3, 3), activation="relu")
+        self.pool2 = layers.MaxPooling2D(pool_size=(2, 2))
+        self.flat = layers.Flatten()
+        self.denses = [layers.Dense(d, activation="relu") for d in dense_units]
+        self.dropout = layers.Dropout(0.5)
+        self.probs = layers.Dense(num_classes, activation="softmax")
 
-	def call(self, inputs):
-		x = self.conv1(inputs)
-		x = self.pool1(x)
-		x = self.conv2(x)
-		x = self.pool2(x)
-		x = self.flat(x)
-		for dense in self.denses:
-			x = dense(x)
-		x = self.dropout(x)
-		return self.probs(x)
+    def call(self, inputs):
+        x = self.conv1(inputs)
+        x = self.pool1(x)
+        x = self.conv2(x)
+        x = self.pool2(x)
+        x = self.flat(x)
+        for dense in self.denses:
+            x = dense(x)
+        x = self.dropout(x)
+        return self.probs(x)
+
 
 class EnsembleUnit(keras.Model):
 
-	def __init__(self, dense_units, num_classes, conv_features = None):
-		super(EnsembleUnit, self).__init__()
-		self.block = EnsembleLayer(dense_units, num_classes, conv_features)
+    def __init__(self, dense_units, num_classes, conv_features=None):
+        super(EnsembleUnit, self).__init__()
+        self.block = EnsembleLayer(dense_units, num_classes, conv_features)
 
-	def call(self, inputs):
-		return self.block(inputs)
+    def call(self, inputs):
+        return self.block(inputs)
+
 
 class EnsembleModel(keras.Model):
 
-	def __init__(self, many_dense_units, num_classes, compile_params, fit_params, many_conv_features = None, trainable = False):
-		super(EnsembleModel, self).__init__()
-		if many_conv_features is None:
-			many_conv_features = [None]*len(many_dense_units)
-		assert len(many_dense_units) == len(many_conv_features)
+    def __init__(self, many_dense_units, num_classes, compile_params,
+                 fit_params, many_conv_features=None, trainable=False):
+        super(EnsembleModel, self).__init__()
+        if many_conv_features is None:
+            many_conv_features = [None] * len(many_dense_units)
+        assert len(many_dense_units) == len(many_conv_features)
 
-		self.many_dense_units = many_dense_units
-		self.num_classes = num_classes
-		self.submodels = [EnsembleUnit(dense_units, num_classes, conv_features) for dense_units, conv_features in zip(many_dense_units, many_conv_features)]
-		[model.compile(**compile_params) for model in self.submodels]
-		# [model.fit(*fit_params[0], **fit_params[1]) for model in self.submodels]
-		for i, model in enumerate(self.submodels):
-			print('Structure of submodel {}:'.format(i+1))
-			print('\tConv shapes: {}'.format(many_conv_features[i])) if not (many_conv_features[i] is None) else print('\tConv shapes: {}'.format(default_conv_features)) 
-			print('\tDense shapes: {}'.format(many_dense_units[i]))
-			model.fit(*fit_params[0], **fit_params[1])
-		for model in self.submodels:
-			model.trainable = False if not trainable else True
+        self.many_dense_units = many_dense_units
+        self.num_classes = num_classes
+        self.submodels = [EnsembleUnit(dense_units, num_classes, conv_features)
+                          for dense_units, conv_features in
+                          zip(many_dense_units, many_conv_features)]
+        [model.compile(**compile_params) for model in self.submodels]
+        for i, model in enumerate(self.submodels):
+            print('Structure of submodel {}:'.format(i + 1))
+            print('\tConv shapes: {}'.format(many_conv_features[i])) if not (
+                        many_conv_features[i] is None) else print(
+                '\tConv shapes: {}'.format(default_conv_features))
+            print('\tDense shapes: {}'.format(many_dense_units[i]))
+            model.fit(*fit_params[0], **fit_params[1])
+        for model in self.submodels:
+            model.trainable = False if not trainable else True
 
-		self.Dense1 = layers.Dense(2*self.num_classes*len(self.many_dense_units), activation = 'relu')
-		self.out_layer = layers.Dense(num_classes, activation = "softmax")
+        self.Dense1 = layers.Dense(
+            2 * self.num_classes * len(self.many_dense_units),
+            activation='relu')
+        self.out_layer = layers.Dense(num_classes, activation="softmax")
 
-	def call(self, inputs):
-		probs = [model(inputs) for model in self.submodels]
-		prob_layer = tf.concat(probs, axis=1)
-		x = self.Dense1(prob_layer)
-		return self.out_layer(x)
+    def call(self, inputs):
+        probs = [model(inputs) for model in self.submodels]
+        prob_layer = tf.concat(probs, axis=1)
+        x = self.Dense1(prob_layer)
+        return self.out_layer(x)
 
-	def get_model_accuracies(self, x_test, y_test):
-		
-		for i, model in enumerate(self.submodels):
-			m = keras.metrics.CategoricalAccuracy()
-			m.update_state(model(x_test), y_test)
-			print(self.many_dense_units[i])
-			print(m.result().numpy())
+    def get_model_accuracies(self, x_test, y_test):
+
+        for i, model in enumerate(self.submodels):
+            m = keras.metrics.CategoricalAccuracy()
+            m.update_state(model(x_test), y_test)
+            print(self.many_dense_units[i])
+            print(m.result().numpy())
