5e3c33d97ff192f84cf4f36a0841a7ec2b41fd00
Matthew Campbell <mmcampbell0@gmail.com>
2020-07-05 16:39:54 -0400

    Erased unnecessary code that was commented, put script into main()

--- dogscats.py
+++ dogscats.py
@@ -37,7 +37,6 @@ file_frac = 1.0
 batch_size = 16
 
 # Model Hyperparameters
-
 main_epochs = 10  # Epochs for meta learner
 sub_epochs = 5  # Epochs for base models
 num_classes = 2  # Number of classes to predict
@@ -59,123 +58,126 @@ conv_shapes = list(map(
 log_dir_sub = "./logs/fit/submodels/" + dt.datetime.now().strftime(
     "%Y%m%d-%H%M%S")
 log_dir_main = "./logs/fit/main/" + dt.datetime.now().strftime("%Y%m%d-%H%M%S")
-# tensorboard_callback_sub = tf.keras.callbacks.TensorBoard(
-#     log_dir=log_dir_sub, histogram_freq=1, profile_batch=0
-# )
 tensorboard_callback_main = tf.keras.callbacks.TensorBoard(
     log_dir=log_dir_main, histogram_freq=1
 )
 
-filenames = os.listdir(train_path)
-categories = []
-for filename in filenames:
-    category = filename.split('.')[0]
-    if category == 'dog':
-        categories.append(1)
-    else:
-        categories.append(0)
+# Main script, data collection and training
+def main():
+    filenames = os.listdir(train_path)
+    categories = []
+    for filename in filenames:
+        category = filename.split('.')[0]
+        if category == 'dog':
+            categories.append(1)
+        else:
+            categories.append(0)
 
-file_df = pd.DataFrame({'filename': filenames, 'category': categories})
-# Filter down samples to allow for dev
-file_df = file_df.sample(frac=file_frac)
-file_df['category'].replace({0: 'cat', 1: 'dog'}, inplace=True)
+    file_df = pd.DataFrame({'filename': filenames, 'category': categories})
+    # Filter down samples to allow for dev
+    file_df = file_df.sample(frac=file_frac)
+    file_df['category'].replace({0: 'cat', 1: 'dog'}, inplace=True)
 
-# Create training, validation, test dfs
-train_val_df, test_df = train_test_split(file_df, test_size=0.1,
-                                         random_state=0)
-train_df, val_df = train_test_split(train_val_df, test_size=0.2,
-                                    random_state=0)
-train_df.reset_index(drop=True, inplace=True)
-val_df.reset_index(drop=True, inplace=True)
+    # Create training, validation, test dfs
+    train_val_df, test_df = train_test_split(file_df, test_size=0.1,
+                                             random_state=0)
+    train_df, val_df = train_test_split(train_val_df, test_size=0.2,
+                                        random_state=0)
+    train_df.reset_index(drop=True, inplace=True)
+    val_df.reset_index(drop=True, inplace=True)
 
-total_train = train_df.shape[0]
-total_validate = val_df.shape[0]
+    total_train = train_df.shape[0]
+    total_validate = val_df.shape[0]
 
-train_datagen = ImageDataGenerator(
-    rotation_range=15,
-    rescale=1. / 255,
-    shear_range=0.1,
-    zoom_range=0.2,
-    horizontal_flip=True,
-    width_shift_range=0.2,  # 0.1
-    height_shift_range=0.2  # 0.1
-)
-train_generator = train_datagen.flow_from_dataframe(
-    train_df,
-    train_path,
-    x_col='filename',
-    y_col='category',
-    target_size=IMAGE_SIZE,
-    class_mode='categorical',
-    batch_size=batch_size
-)
-val_datagen = ImageDataGenerator(rescale=1. / 255)
-val_generator = val_datagen.flow_from_dataframe(
-    val_df,
-    train_path,
-    x_col='filename',
-    y_col='category',
-    target_size=IMAGE_SIZE,
-    class_mode='categorical',
-    batch_size=batch_size
-)
+    train_datagen = ImageDataGenerator(
+        rotation_range=15,
+        rescale=1. / 255,
+        shear_range=0.1,
+        zoom_range=0.2,
+        horizontal_flip=True,
+        width_shift_range=0.2,  # 0.1
+        height_shift_range=0.2  # 0.1
+    )
+    train_generator = train_datagen.flow_from_dataframe(
+        train_df,
+        train_path,
+        x_col='filename',
+        y_col='category',
+        target_size=IMAGE_SIZE,
+        class_mode='categorical',
+        batch_size=batch_size
+    )
+    val_datagen = ImageDataGenerator(rescale=1. / 255)
+    val_generator = val_datagen.flow_from_dataframe(
+        val_df,
+        train_path,
+        x_col='filename',
+        y_col='category',
+        target_size=IMAGE_SIZE,
+        class_mode='categorical',
+        batch_size=batch_size
+    )
 
-compile_params = {
-    'loss': "categorical_crossentropy",
-    'optimizer': 'RMSprop',
-    'metrics': ['accuracy']
-}
-fit_params = (
-    [train_generator],
-    {'epochs': sub_epochs,
-     'validation_data': val_generator,
-     'validation_steps': total_validate // batch_size,
-     'steps_per_epoch': total_train // batch_size,
-     'verbose': 2,
-     # 'callbacks': [tensorboard_callback_sub],
-     }
-)
-fit_params2 = (
-    [train_generator],
-    {'epochs': main_epochs,
-     'validation_data': val_generator,
-     'validation_steps': total_validate // batch_size,
-     'steps_per_epoch': total_train // batch_size,
-     'verbose': 2,
-     'callbacks': [tensorboard_callback_main],
-     }
-)
+    compile_params = {
+        'loss': "categorical_crossentropy",
+        'optimizer': 'RMSprop',
+        'metrics': ['accuracy']
+    }
+    fit_params = (
+        [train_generator],
+        {'epochs': sub_epochs,
+         'validation_data': val_generator,
+         'validation_steps': total_validate // batch_size,
+         'steps_per_epoch': total_train // batch_size,
+         'verbose': 2,
+         # 'callbacks': [tensorboard_callback_sub],
+         }
+    )
+    fit_params2 = (
+        [train_generator],
+        {'epochs': main_epochs,
+         'validation_data': val_generator,
+         'validation_steps': total_validate // batch_size,
+         'steps_per_epoch': total_train // batch_size,
+         'verbose': 2,
+         'callbacks': [tensorboard_callback_main],
+         }
+    )
 
-model = EnsembleModel(
-    dense_shapes, num_classes, compile_params,
-    fit_params, conv_shapes, trainable=False,
-    logging=log_dir_sub
-)
-model.compile(**compile_params)
-model.fit(*fit_params2[0], **fit_params2[1])
+    model = EnsembleModel(
+        dense_shapes, num_classes, compile_params,
+        fit_params, conv_shapes, trainable=False,
+        logging=log_dir_sub
+    )
+    model.compile(**compile_params)
+    model.fit(*fit_params2[0], **fit_params2[1])
 
-test_datagen = ImageDataGenerator(rescale=1. / 255)
-test_generator = test_datagen.flow_from_dataframe(
-    test_df,
-    train_path,  # A little bit unorthodox, but we only have labeled train
-    # images
-    x_col='filename',
-    y_col=None,
-    class_mode=None,
-    target_size=IMAGE_SIZE,
-    batch_size=batch_size,
-    shuffle=False
-)
+    test_datagen = ImageDataGenerator(rescale=1. / 255)
+    test_generator = test_datagen.flow_from_dataframe(
+        test_df,
+        train_path,  # A little bit unorthodox, but we only have labeled train
+        # images
+        x_col='filename',
+        y_col=None,
+        class_mode=None,
+        target_size=IMAGE_SIZE,
+        batch_size=batch_size,
+        shuffle=False
+    )
 
 
-# Model Assessment
-def model_accuracy(test_generator) -> float:
-    pred_class = model.predict(test_generator)
-    pred = np.round(pred_class[:, 1])
-    test_df['category'].replace(['cat', 'dog'], [0, 1], inplace=True)
-    truth = test_df['category']
-    accuracy = np.mean(pred == truth)
-    return accuracy
+    # Model Assessment
+    def model_accuracy(test_generator) -> float:
+        pred_class = model.predict(test_generator)
+        pred = np.round(pred_class[:, 1])
+        test_df['category'].replace(['cat', 'dog'], [0, 1], inplace=True)
+        truth = test_df['category']
+        accuracy = np.mean(pred == truth)
+        return accuracy
 
-accuracy = model_accuracy(test_generator)
-print("Model Accuracy: ", accuracy)
+    accuracy = model_accuracy(test_generator)
+    print("Model Accuracy: ", accuracy)
+    return model
+
+if __name__ == '__main__':
+    model = main()
